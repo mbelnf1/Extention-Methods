dff5a0fc05b1499d457687a70a38dfabcfc10685
mbelnf1 <mbeln1992@gmail.com>
2019-08-05 15:25:52 -0400

    Add project files.

---
+++ Demonstrations/Converters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demonstrations
+{
+    public static class Converters
+    {
+        public static double ConvertStringToDecimal(string input)
+        {
+            double output = 0;
+
+            double.TryParse(input, out output);
+
+            return output;
+        }
+
+        public static double ToDouble(this string input)
+        {
+            double output = 0;
+
+            double.TryParse(input, out output);
+
+            return output;
+        }
+    }
+    }
+}
---
+++ Demonstrations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demonstrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x: ");
+            string firstValue = Console.ReadLine();
+
+            Console.Write("y: ");
+            string secondValue = Console.ReadLine();
+
+            double x = Converters.ConvertStringToDecimal(firstValue);
+            double y = secondValue.ToDouble();
+
+            double z = x * y;
+
+            Console.WriteLine("The product of {0} and {1} is {2}", x, y, z);
+
+            Console.ReadLine();
+        }
+    }
+}
